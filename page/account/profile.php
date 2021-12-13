ecdcb4ab8bb7517d5c1ccfccbda71bfa5c5c8ef4
alpaca0x0 <alpaca0x0.tw@gmail.com>
2021-12-13 21:57:33 +0800

    done show session life

--- page/account/profile.php
+++ page/account/profile.php
@@ -18,7 +18,9 @@ $User->Is('login') OR exit(header("Location: ".Root('index')));
 	<p ref="id">ID: {{ user.id }}</p>
 	<p ref="username">Username: {{ user.name }}</p>
 	<p ref="identity">Identity: {{ user.identity }}</p>
-	<p ref="identity">Token: {{ user.token }}</p>
+	<p ref="token">Spawn Time: {{ user.spawntime }}</p>
+	<p ref="token">Token: {{ user.token }}</p>
+	<p ref="timeout">Session Life: {{ user.life.h }}:{{ user.life.i }}:{{ user.life.s }}</p>
 </div>
 
 <script type="module">
@@ -29,11 +31,21 @@ $User->Is('login') OR exit(header("Location: ".Root('index')));
 				id: '<?php echo $User->Get('id'); ?>',
 				name: '<?php echo htmlentities($User->Get('name',' - ')); ?>',
 				identity: '<?php echo htmlentities($User->Get('identity')); ?>',
-				token: '<?php echo $User->Get('token',' - '); ?>'
-			}
+				token: '<?php echo $User->Get('token',' - '); ?>',
+				spawntime: '<?php echo $User->Get('spawntime',' - '); ?>',
+				life: { h: "-", i: "-", s: "-", },
+			},
+			timeout: 60*60*6,
 		}},
 		mounted(){
-			//
+			let timeout = this.user.spawntime, t, currentTime;
+			setInterval(() => {
+				currentTime = new Date().getTime() / 1000;
+				t = this.timeout - (currentTime - timeout);
+				this.user.life.h = parseInt(t / 60 / 60);
+				this.user.life.i = parseInt((t / 60) % 60);
+				this.user.life.s = parseInt(t % 60);
+			},1000);
 		}
 	}).mount('#Profile');
 </script>
