f5983e1f34e692ed8fe4871238dc5b3985810563
alpaca0x0 <alpaca0x0.tw@gmail.com>
2021-12-13 21:55:53 +0800

    add `user.conf`

--- function/user.func.php
+++ function/user.func.php
@@ -1,9 +1,10 @@
 <?php defined('INIT') or die('NO INIT'); ?>
 
 <?php
+$config = @include_once(Conf('user'));
 @include_once(Clas('user'));
 ?>
 
 <?php
 $User = new User();
-$User->Init();
+$User->Init($config);
