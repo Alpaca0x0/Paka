93f7ae723457337b45caa3fcbd152a59ab10573f
Alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-01-07 01:59:12 +0800

    updated

--- asset/js/init.js
+++ asset/js/init.js
@@ -1,16 +1,14 @@
-window.A = new Array();
-
-A.setCookie = function(name,value,days) {
+function setCookie(name,value,minutes=60*7){
     var expires = "";
-    if (days) {
+    if(minutes){
         var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
+        date.setTime(date.getTime() + minutes*60*1000);
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
-A.getCookie = function(name) {
+function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
@@ -21,37 +19,6 @@ A.getCookie = function(name) {
     return null;
 }
 
-A.qsa = function(target){
-	eles = document.querySelectorAll(target)
-	eles.forEach((ele) => {
-	//
-		ele.attr = ele.getAttribute;
-		ele.hasAttr = ele.hasAttribute;
-		ele.setAttr = ele.setAttribute;
-		ele.removeAttr = ele.removeAttribute;
-	//
-	});
-	return eles;
+function delCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
-
-A.qs = function(target){
-	let ele = document.querySelector(target);
-	//
-	ele.attr = ele.getAttribute;
-	ele.hasAttr = ele.hasAttribute;
-	ele.setAttr = ele.setAttribute;
-	ele.removeAttr = ele.removeAttribute;
-	// ele.listen = $.listen(ele);
-	// ele.prototype.prop
-	//
-	return ele;
-}
-
-A.listen = new Array();
-A.listen.attr = function(ele,callback){
-	let observer = new MutationObserver(callback);
-	observer.observe(ele, {attributes:true});
-	return observer;
-}
-
-
--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php @include_once('init.php'); ?>
 
-<?php
-
-@include_once(Func('post'));
-// $result = $Post->RemoveComment(126);
-
-// var_dump($result);
+<script type="text/javascript">
+	console.log(navigator.language);
+</script>
