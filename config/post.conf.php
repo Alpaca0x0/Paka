9300fb879e37401adca7d803857b090fe45604ff
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-02-03 15:32:07 +0800

    add post config(setting length)

---
+++ config/post.conf.php
@@ -0,0 +1,17 @@
+<?php defined('INIT') or die('NO INIT'); ?>
+
+<?php
+return [
+    'title' => [
+    	'min' => 2,
+    	'max' => 64,
+    ],
+    'content' => [
+    	'min' => 2,
+    	'max' => 535,
+    ],
+    'comment' => [
+    	'min' => 2,
+    	'max' => 535,
+    ],
+];
