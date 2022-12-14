479661b66949dc99b4e71dfb0e2e1722517eff7f
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-14 12:21:32 +0800

    example config files

---
+++ config.example.php
@@ -0,0 +1,26 @@
+<?php
+
+################################################################
+# Usually needed to be set      ##
+##################################
+
+# Debug mode, show errors
+define('DEBUG', true);
+
+# Development mode, show info
+define('DEV', true);
+
+# Development mode, show more info
+define('FULL_DEV', false);
+
+# Root path in URL, e.g. /router/
+define('Root', '/paka/');
+
+################################################################
+# Optional settings             ##
+##################################
+
+# Local path of project, it's not needed to change usually
+define('Local', __DIR__.DIRECTORY_SEPARATOR);
+
+################################################################
