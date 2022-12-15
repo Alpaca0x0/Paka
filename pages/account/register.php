0068fa04c6ed2b825e2497e7036151ae253cc3b1
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-15 20:21:36 +0800

    doing ui, almost done (not including ajax)

--- pages/account/register.php
+++ pages/account/register.php
@@ -21,59 +21,94 @@ $config = Inc::config('account');
 <div class="ts-space is-big"></div>
 
 <div id="Register" class="ts-container is-very-narrow">
-    <div class="ts-text is-label">使用者帳號</div>
-    <div class="ts-space"></div>
-    <div class="ts-input is-negative is-underlined is-fluid">
-        <input type="text">
-    </div>
-    <div class="ts-space is-small"></div>
-    <div class="ts-text is-description"><?=htmlentities($config['description']['username'])?></div>
-
-    <div class="ts-space is-large"></div>
     <div class="ts-text is-label">電子郵件地址</div>
     <div class="ts-space"></div>
-    <div class="ts-input is-underlined is-fluid">
-        <input type="text">
+    <div class="ts-input is-underlined is-fluid" :class="classObject('email')">
+        <input @input="checkDatas()" :readonly="is.submitting" type="email" v-model="fields.email.value" :ref="setRef" id="email" v-focus>
+    </div>
+    <div class="ts-space is-small"></div>
+    <div class="ts-text is-description"><?=htmlentities($config['description']['email'])?></div>
+    
+    <div class="ts-space is-large"></div>
+    <div class="ts-grid is-stackable">
+        <div class="column is-7-wide">
+            <div class="ts-text is-label">使用者帳號</div>
+            <div class="ts-space"></div>
+            <div class="ts-input is-underlined is-fluid" :class="classObject('username')">
+                <input @input="checkDatas()" :readonly="is.submitting" type="text" v-model="fields.username.value" :ref="setRef" id="username">
+            </div>
+            <div class="ts-space is-small"></div>
+            <div class="ts-text is-description"><?=htmlentities($config['description']['username'])?></div>
+        </div>
+        <div class="column is-9-wide">
+            <div class="ts-text is-label">密碼</div>
+            <div class="ts-space"></div>
+            <div class="ts-input is-underlined is-fluid" :class="classObject('password')">
+                <input @input="checkDatas()" :readonly="is.submitting" type="password" v-model="fields.password.value" :ref="setRef" id="password">
+            </div>
+            <div class="ts-space is-small"></div>
+            <div class="ts-text is-description"><?=htmlentities($config['description']['password'])?></div>
+        </div>
     </div>
 
     <div class="ts-space is-large"></div>
-    <div class="ts-text is-label">密碼</div>
-    <div class="ts-space"></div>
-    <div class="ts-input is-underlined is-fluid">
-        <input type="password">
-    </div>
-    <div class="ts-space is-small"></div>
-    <div class="ts-text is-description"><?=htmlentities($config['description']['password'])?></div>
-    
+    <div class="ts-divider is-center-text">為防止機器腳本註冊，需進行驗證</div>
     <div class="ts-space is-large"></div>
-    <div class="ts-text is-label">驗證碼</div>
-    <img :src="fields.captcha.src" @click="fields.captcha.change()" style="cursor: pointer;">
-    <div :class="classObject('captcha')" @input="checkDatas()" class="ts-input is-start-icon is-underlined is-fluid">
-        <span class="ts-icon is-robot-icon"></span>
-        <input type="text" ref="refCaptcha" v-model="fields.captcha.value" :readonly="is.submitting" maxlength="6">
+
+    <div class="ts-grid is-stackable">
+        <div class="column is-7-wide is-center-aligned">
+            <!-- <div class="ts-text is-label">驗證圖</div> -->
+            <!-- <div class="ts-space"></div> -->
+            <div class="ts-image is-rounded">
+                <img :src="fields.captcha.src" @click="fields.captcha.change()" style="cursor: pointer; max-width:100%;">
+            </div>
+            <div class="ts-space is-small"></div>
+            <div class="ts-text is-description">(若看不清楚，可點擊圖示更換)</div>
+        </div>
+        
+        <div class="column is-9-wide">
+            <div class="ts-text is-label">驗證碼</div>
+            <div class="ts-space"></div>
+            <div :class="classObject('captcha')" @input="checkDatas()" class="ts-input is-start-icon is-underlined is-fluid">
+                <span class="ts-icon is-robot-icon"></span>
+                <input @input="checkDatas()" :readonly="is.submitting" type="text" v-model="fields.captcha.value" :ref="setRef" id="captcha" maxlength="6">
+            </div>
+            <div class="ts-space is-small"></div>
+            <div class="ts-text is-description"><?=htmlentities($config['description']['captcha'])?></div>
+        </div>
     </div>
 
 
     <div class="ts-space is-large"></div>
     <button class="ts-button is-fluid">下一步</button>
     <div class="ts-space is-small"></div>
-    <div class="ts-text is-center-aligned is-description">按下「下一步」表示您也接受「伊繁星最高協議」、「個人隱私政策」、「使用者規範」。</div>
+    <!-- <div class="ts-text is-center-aligned is-description">按下「下一步」表示您也接受「伊繁星最高協議」、「個人隱私政策」、「使用者規範」。</div> -->
 </div>
 
+<div class="ts-space"></div>
+
 <script type="module">
-    import { createApp, reactive, ref } from '<?=Uri::js('vue')?>';
+    import { createApp, reactive, ref, nextTick, onMounted } from '<?=Uri::js('vue')?>';
     import * as directives from '<?=Uri::js('vue/directives')?>';
 
     const Register = createApp({setup(){
+        let refs = reactive({});
+        let setRef = (el) => { refs[el.id] = el; }
+        // 
         let is = reactive({
             submit: false,
         });
         // 
         let fields = reactive({
+            email: {
+                status: 'warning', 
+                value: '<?=(DEV)?'gzmalxnsk@gmail.com':''?>',
+                regex: [<?=$config['email']?>],
+            },
             username: {
                 status: 'warning', 
                 value: '<?=(DEV)?'alpaca0x0':''?>',
-                regex: [<?=$config['username']?>,<?=$config['email']?>],
+                regex: [<?=$config['username']?>],
             },
             password: {
                 status: 'warning',
@@ -100,6 +135,10 @@ $config = Inc::config('account');
         // 
         const classObject = (key) => {
             let objects = {
+                email: {
+                    'is-negative': fields.email.status==='warning',
+                    'is-disabled': is.submitting,
+                },
                 username: {
                     'is-negative': fields.username.status==='warning',
                     'is-disabled': is.submitting,
@@ -118,7 +157,23 @@ $config = Inc::config('account');
                 }
             };
             return objects[key];
-        }
+        };
+        // 
+        const checkDatas = () => {
+            let isPass = true;
+            Object.values(fields).forEach((field) => {
+                if(typeof(field.value) === undefined || typeof(field.regex) === undefined){ return; }
+                if(!Array.isArray(field.regex)){ field.regex = [field.regex]; }
+                let isMatch = (field.regex).some((regex)=>(field.value).match(regex));
+                if(!isMatch && !field.optional){
+                    isPass = false;
+                    if(typeof(field.status) !== undefined){ field.status = 'warning'; }
+                }else{
+                    if(typeof(field.status) !== undefined){ field.status = 'success'; }
+                }
+            });
+            return isPass;
+        };
         //  
         const Dev = {
             getCaptcha: () => {
@@ -139,7 +194,11 @@ $config = Inc::config('account');
             msg: null,
         });
         // 
-        return { is, fields, classObject };
+        onMounted(() => {
+            checkDatas();
+        });
+        // 
+        return { is, refs, fields, classObject, info, checkDatas };
     }}).directive('focus',
         directives.focus
     ).mount('#Register');
