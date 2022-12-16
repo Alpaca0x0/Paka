bd95a85909ed62d1cf289a10c84e96d8c7315f15
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-16 17:49:02 +0800

    fix bug, `isset()` will return false when value of variable is null, change it to `array_key_exists()`

--- classes/init/arr.php
+++ classes/init/arr.php
@@ -3,14 +3,14 @@ class Arr{
     static function every($array){
         $args = func_get_args();
         array_shift($args);
-        foreach ($args as $arg) { if(!isset($array[$arg])) return false; }
+        foreach ($args as $arg) { if(!array_key_exists($arg, $array)) return false; }
         return true;
     }
 
     static function includes($array){
         $args = func_get_args();
         array_shift($args);
-        foreach ($args as $arg) { if(isset($array[$arg])) return true; }
+        foreach ($args as $arg) { if(array_key_exists($arg, $array)) return true; }
         return false;
     }
 }
