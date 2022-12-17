e312ab889c50662eda84b77eebd8d82cd39c08ba
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-17 23:26:25 +0800

    beautify return datas format in class

--- classes/forum.php
+++ classes/forum.php
@@ -40,8 +40,7 @@ class Forum{
                 JOIN `profile` ON (`post`.`poster`=`profile`.`id`) 
                 LEFT JOIN `post_edited` ON (`post`.`id`=`post_edited`.`post`) 
                 WHERE `post`.`status`=:status AND `post`.`id` > :after AND `post`.`id` < :before
-                GROUP BY `post`.`id`
-        ";
+                GROUP BY `post`.`id`";
         $sql .= is_null($orderBy) ? '' : " ORDER BY $orderBy[0] $orderBy[1] ";
         $sql .= " LIMIT {$limit}";
         $sql .= ';';
@@ -54,6 +53,92 @@ class Forum{
         if(DB::error()){ return false; }
         $row = DB::fetchAll();
         if(!$row){ return null; }
-        return $row;
+        // 
+        $ret = [];
+        foreach ($row as $key => $val) {
+            array_push($ret, [
+                'id' => (int)$val['id'],
+                'title' => $val['title'],
+                'content' => $val['content'],
+                'datetime' => $val['datetime'],
+                'poster' => [
+                    'id' => $val['poster'],
+                    'username' => $val['poster_username'],
+                    'identity' => $val['poster_identity'],
+                    'nickname' => $val['profile_nickname'],
+                    'gender' => $val['profile_gender'],
+                    'avatar' => !is_null($val['profile_avatar'])?base64_encode($val['profile_avatar']):null,
+                ],
+                'edited' => [
+                    'times' => $val['post_edited_times'],
+                    'last_time' => $val['post_edited_datetime'],
+                ],
+            ]);
+        }
+        return $ret;
+    }
+
+    static function getPost($pid){
+        $sql = "SELECT `post`.`id`,`post`.`title`,`post`.`content`,`post`.`poster`,`post`.`datetime`,
+                `account`.`username`as`poster_username`, `account`.`identity`as`poster_identity`,
+                `profile`.`nickname`as`profile_nickname`, `profile`.`gender`as`profile_gender`, `profile`.`avatar`as`profile_avatar`,
+                COUNT(`post_edited`.`id`)as`post_edited_times`, MAX(`post_edited`.`datetime`)as`post_edited_datetime` 
+                FROM `post` 
+                JOIN `account` ON (`post`.`poster`=`account`.`id`) 
+                JOIN `profile` ON (`post`.`poster`=`profile`.`id`) 
+                LEFT JOIN `post_edited` ON (`post`.`id`=`post_edited`.`post`) 
+                WHERE `post`.`status`=:status AND `post`.`id` = :pid
+                GROUP BY `post`.`id`
+                LIMIT 1;";
+        // 
+        DB::query($sql)::execute([
+            ':status' => "alive",
+            ':pid' => $pid,
+        ]);
+        if(DB::error()){ return false; }
+        $post = DB::fetch();
+        if(!$post){ return null; }
+        // 
+        $ret = [
+            'id' => (int)$post['id'],
+            'title' => $post['title'],
+            'content' => $post['content'],
+            'datetime' => $post['datetime'],
+            'poster' => [
+                'id' => $post['poster'],
+                'username' => $post['poster_username'],
+                'identity' => $post['poster_identity'],
+                'nickname' => $post['profile_nickname'],
+                'gender' => $post['profile_gender'],
+                'avatar' => !is_null($post['profile_avatar'])?base64_encode($post['profile_avatar']):null,
+            ],
+            'edited' => [
+                'times' => $post['post_edited_times'],
+                'last_time' => $post['post_edited_datetime'],
+            ],
+        ];
+        return $ret;
+    }
+
+    static function deletePost($pid){
+        DB::beginTransaction();
+        // delete post
+		$sql = "UPDATE `post` SET `status`=:status WHERE `id`=:pid;";
+        DB::query($sql)::execute([
+            ':status' => 'removed',
+            ':pid' => $pid,
+        ]);
+		if(DB::error()){ DB::rollback(); return false; }
+        $rowCount = DB::rowCount();
+        // delete comment
+        $sql = "UPDATE `comment` SET `status`=:status WHERE `post`=:pid;";
+        DB::query($sql)::execute([
+            ':status' => 'removed',
+            ':pid' => $pid,
+        ]);
+		if(DB::error()){ DB::rollback(); return false; }
+        // done
+        DB::commit();
+        return $rowCount;
     }
 }
