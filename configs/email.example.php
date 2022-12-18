c5ccb951138ff706499a3c474e2a141efe7893c3
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-18 20:53:26 +0800

    google smtp is great :D

--- configs/email.example.php
+++ configs/email.example.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'host' => 'smtp.example.com',
-	'port' => 465,
+	'host' => 'smtp.gmail.com',
+	'port' => 587,
 	'email' => 'service@example.com',
 	'pass' => 'password',
 	'name' => 'Admin'
