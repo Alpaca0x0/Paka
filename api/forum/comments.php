a57eb27d814bf48fc56601fba388ca8d9ea44c89
alpaca0x0 <alpaca0x0.tw@gmail.com>
2022-12-29 16:51:51 +0800

    replace `is_null()` to `!`

--- api/forum/comments.php
+++ api/forum/comments.php
@@ -63,7 +63,7 @@ $comments = Forum::before($before)
 				::getComments($pids);
 // 
 if($comments === false){ Resp::error('sql_query', 'SQL 語法查詢失敗'); }
-if(is_null($comments)){ Resp::success('empty_data', null, '查詢成功，但資料為空'); }
+if(!$comments){ Resp::success('empty_data', null, '查詢成功，但資料為空'); }
 if(!is_array($comments)){ Resp::error('sql_query_return_format', 'SQL 語法查詢返回錯誤格式'); }
 
 foreach($comments as $idx => $comment){
